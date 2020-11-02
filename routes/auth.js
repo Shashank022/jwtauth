d52865826e882bf41bab2bf5c16bc507ec8a9e8f
Shashank <shashank022@gmail.com>
2020-11-02 14:01:08 -0600

    Added chnages

--- routes/auth.js
+++ routes/auth.js
@@ -1,41 +1,15 @@
 const router = require('express').Router();
 const Joi = require('joi');
 const User = require('../model/User');
-
-const schema =  Joi.object().keys({
-    name : Joi.string().alphanum().min(6).required(),
-    email : Joi.string().min(6).required().email(),
-    password : Joi.string().min(6).required(),
-});
+const validation = require('../validation');
 
 router.get('/all', async (req,res)=>{
     res.send("Welcome to the GET Method");
 });
 
 router.post('/register', async (req,res)=>{
-    
-    const user = new User({
-        name: req.body.name,
-        email : req.body.email,
-        password : req.body.password
-    });
-        // schema options
-        const options = {
-            abortEarly: false, // include all errors
-            allowUnknown: true, // ignore unknown props
-            stripUnknown: true // remove unknown props
-        };
-
-       // validate request body against schema
-       const { error, value } = schema.validate(user, options);
-    
-       if (error) {
-           res.send(`Validation error: ${error.details.map(x => x.message).join(', ')}`);
-       } else {
-        const savedUser = await user.save();
-        res.send(savedUser);
-       }
-
+    const result = await validation.userValidation(req, res);
+    res.send(result);
 });
 
 module.exports = router;
--- validation.js
+++ validation.js
@@ -1,5 +1,6 @@
 const Joi = require('joi');
 const validator = require('express-joi-validation').createValidator({});
+const User = require('./model/User');
 
 const registerValidation  =  data => {
     const schema = Joi.object( {
@@ -21,6 +22,35 @@ const loginValidation = data =>{
     return schema;
 };
 
-module.exports = { registerValidation,loginValidation }
+const userValidation  =  (req, res) => {
+    const schema =  Joi.object().keys({
+        name : Joi.string().alphanum().min(6).required(),
+        email : Joi.string().min(6).required().email(),
+        password : Joi.string().min(6).required(),
+    });
+
+    const user = new User({
+        name: req.body.name,
+        email : req.body.email,
+        password : req.body.password
+    });
+    // schema options
+    const options = {
+        abortEarly: false, // include all errors
+        allowUnknown: true, // ignore unknown props
+        stripUnknown: true // remove unknown props
+    };
+       
+    const { error, value } = schema.validate(user, options);
+
+    if (error) {
+        res.send(`Validation error: ${error.details.map(x => x.message).join(', ')}`);
+    } else {
+        const savedUser = user.save();
+        return savedUser;
+    }
+};
+
+module.exports = { registerValidation,loginValidation, userValidation }
 
 
